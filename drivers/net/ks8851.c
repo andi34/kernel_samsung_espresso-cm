ef55511fc59aebf0a67933615a04ab15fa9b0c71
Andreas B <skate4life@gmx.de>
2014-07-07 22:05:19 +0000

    Remove Whitespaces

--- drivers/net/ks8851.c
+++ drivers/net/ks8851.c
@@ -1398,7 +1398,6 @@ static int ks8851_set_eeprom(struct net_device *dev,
 		eeprom_buff[last_word - first_word] =
 					ks8851_eeprom_read(dev, last_word);
 
-
 	/* Device's eeprom is little-endian, word addressable */
 	le16_to_cpus(&eeprom_buff[0]);
 	le16_to_cpus(&eeprom_buff[last_word - first_word]);
@@ -1678,7 +1677,6 @@ static int __devinit ks8851_probe(struct spi_device *spi)
 		    ndev->dev_addr, ndev->irq);
 
 	return 0;
-
 
 err_netdev:
 	free_irq(ndev->irq, ndev);

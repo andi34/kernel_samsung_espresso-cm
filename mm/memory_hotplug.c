2dee873f136bb2efcf4a679ffb7413b814e32536
andi34 <skate4life@gmx.de>
2014-07-14 23:33:25 +0200

    partially remove 3.0.38-39 upstream changes
    mm: compaction: determine if dirty pages can be migrated without blocking within ->migratepage
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=397d9c507ff1c9c5afc80c80ee245c2455d6a1db
    
    mm: compaction: introduce sync-light migration for use by compaction
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=f869774c37710ef2b773d167d184b9936988d07f

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -749,7 +749,7 @@ do_migrate_range(unsigned long start_pfn, unsigned long end_pfn)
 		}
 		/* this function returns # of failed pages */
 		ret = migrate_pages(&source, hotremove_migrate_alloc, 0,
-							true, MIGRATE_SYNC);
+								true, true, 0);
 		if (ret)
 			putback_lru_pages(&source);
 	}
